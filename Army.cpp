e2d72ed26db2ecfb23d05bb86c885f4ae4159eda
MALAK955 <130931280+MALAK955@users.noreply.github.com>
2026-02-08 18:11:43 +0200

    Update Army.cpp

--- Army.cpp
+++ Army.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 int main() {
     int n;cin >> n;
-    int arr[105] = {0};
+    int arr[99] = {0};
     for (int i = 0;i < n - 1;i++) {
         cin >> arr[i];
     }
     int a, b;
     cin >> a >> b;
-    int min = a - b;
+    int min = abs(a - b);
     ////result = sum of arr[i]
     int result = 0;
-    for (int i = a - 1;i < b - 1;i++) {
+    for (int i = 0;i <= min - 1;i++) {
         result += arr[i];
     }
         cout << result;
